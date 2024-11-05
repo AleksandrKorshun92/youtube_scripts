5fac7f8cc3c70c0f1ae7ed9f1f5382e4f08f0aec
aleksander <spaik1192@mail.ru>
2024-11-05 07:54:30 +0300

    добавил асинхронности для получения данных о видео и улучшил логирование

--- youtube.py
+++ youtube.py
@@ -1,27 +1,41 @@
-# сделать try/ex + описание проекта
+""" Этот код представляет собой пример использования Python для взаимодействия с YouTube Data API
+и Google Drive API. Основная цель программы заключается в поиске видео на YouTube по заданному 
+ключевому слову, сборе информации о найденных видео и сохранении этих данных в CSV-файл.
+Кроме того, программа также может загружать данные в Google Диск при наличии соответствующих 
+прав доступа.
 
-import asyncio
-import csv
-import os
-import requests
-import logging
+Импортированные модули:
+- google.oauth2: Модуль для доступа к API Google работы с аутентификацией и авторизацией через протокол OAuth 2.0
+- googleapiclient: Модуль для взаимодействия с API Google.
+- asyncio: Асинхронная библиотека Python.
+- csv: Модуль для работы с файлами формата CSV (запись, чтение).
+- aiohttp: Библиотека для асинхронного HTTP-клиента (для отправки Api запросов)
+- requests: Модуль для для выполнения HTTP-запросов.
+- logging: Модуль для логирования. 
+"""
+
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+import asyncio
+import csv
+import aiohttp
+import requests
+import logging
 
- 
-# Настройки YouTube Data API  our_youtube_api_key
-YOUTUBE_API_KEY = 'Ar_youtube_api_key'  # заменить на Ваш API_KEY
+
+# Настройки констант для YouTube Data API 
+YOUTUBE_API_KEY = 'our_youtube_api_key'  # заменить на Ваш API_KEY
 YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3/search"
 VIDEO_URL = 'https://www.googleapis.com/youtube/v3/videos'
 
-# Настройки Google Drive
-SERVICE_ACCOUNT_FILE = 'path/to/your/credentials.json'  # заменить на путь к файлу с данными SERVICE_ACCOUNT_FILE
-SCOPES = ['https://www.googleapis.com/auth/drive.file'] # права доступа 
+# Настройки констант для Google Drive
+SERVICE_ACCOUNT_FILE = 'path/to/your/credentials.json'  # заменить на путь к файлу с данными 
+SCOPES = ['https://www.googleapis.com/auth/drive.file'] 
 
 
-# Настройка логгирования. Данные хранятся в файле api.log с уровнем логирования INFO. 
+# Настройка логгирования. Данные хранятся в файле youtube.log с уровнем логирования INFO. 
 logging.basicConfig(
     filename = 'youtube.log',  
     level = logging.INFO,  
@@ -30,10 +44,16 @@ logging.basicConfig(
 
 
 def search_youtube(query: str, maxResults: int = 50) -> dict:
-    """Ищет видео на YouTube по ключевому слову."""
+    """Функция ищет видео на YouTube по ключевому слову.
+    
+    :param query: ключевое слово для поиска видео.
+    :param maxResults: максимальное количество видео установлено по умолчанию 50.
+    :return: словарь с информацией о найденных видео.
+    """
     
     logging.info(f'start search - {query}')
     
+    # основные параметр для получения ланных о видео
     params = {
         'part': 'snippet',
         'q': query,
@@ -45,20 +65,36 @@ def search_youtube(query: str, maxResults: int = 50) -> dict:
         response = requests.get(YOUTUBE_API_URL, params=params)
         return response.json()
     except Exception as e:
-        logging.error(e)
+        logging.exception("Error executing request: %s", e)
+        return {}
+
 
 async def fetch_video_details(video_id: str) -> dict:
-    """Получает детали видео по его ID."""
+    """Асинкронная функция, которая получает детали видео по его ID.
+    
+    :param video_id: id видео для получения данных по данному видео.
+    :return: словарь с расширенной информацией о видео.
+    """
+    
     logging.info(f'start function fetch_video_details')
     video_details_url = f'{VIDEO_URL}?part=statistics,snippet&id={video_id}&key={YOUTUBE_API_KEY}'
     try: 
-        response = requests.get(video_details_url)
-        return response.json()
-    except Exception as e:
-        logging.error(e)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(video_details_url) as response:
+                return await response.json()
+    except aiohttp.ClientError as e:
+        logging.error(f"Error executing request: {e}")
+        return {}
+
 
 async def gather_video_info(video_data: dict) -> dict:
-    """Собирает информацию о каждом видео."""
+    """Асинкронная функция которая собирает информацию о каждом видео для дальнейшей записи.
+    В цикле прохлдит по каждому видео, находит id и через функцию fetch_video_details получает
+    расширинные данные необходимые для дальнейшей записи (обработки)
+    
+    :param video_data: словарь найденных видео
+    :return: словарь с расширенной информацией о видео.
+    """
     logging.info('start gather_video_info')
     
     tasks = []
@@ -69,12 +105,24 @@ async def gather_video_info(video_data: dict) -> dict:
         video_details = await asyncio.gather(*tasks)
         return video_details
     except Exception as e:
-        logging.error(e)
+        logging.error(f"Error executing request: {e}")
+        return {}
 
-def save_to_csv(video_details: dict, filename: str):
-    """Сохраняет информацию о видео в CSV файл."""
-    logging.info(f'save_to_csv filename - {filename}')
+
+def save_to_csv(video_details: dict, filename: str) -> str:
+    """Сохраняет информацию о видео в CSV файл.
     
+    :param video_details: словарь данных о видео
+    :param filename: названия файла, куда будет сохранятся информация
+    :return: информацию, что данные сохранены в файл.
+    """
+    logging.info(f'save_to_csv filename - {filename}') 
+    
+    # проверка на правильный формат для сохранения файла. Если формата нет или другой, 
+    # то будет изменен формат
+    if 'csv' not in filename.split('.'):
+        filename = filename + '.csv'
+    #открытие менеджера для записи с указанием кодировки utf-8.
     with open(filename, mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(['Title (Название видео)', 
@@ -93,28 +141,18 @@ def save_to_csv(video_details: dict, filename: str):
                     statistics.get('likeCount', 0),
                     statistics.get('commentCount', 0)
                 ])
+    return f'Данные о видео сохранены в {filename}'
 
 
-async def main():
-    """Основная функция."""
+
+def upload_to_drive(filename: csv) -> str:
+    """Загружает файл в c данными о видео в Google Drive.
     
-    query = input("Введите ключевое слово для поиска: ")
-    video_data = search_youtube(query)
-    try:
-        if video_data:
-            video_info = await gather_video_info(video_data)
+    :param filename: названия файла, который необходимо сохранить в Google Drive
+    :return: информацию, что данные сохранены в файл.
+    """
     
-            save_to_csv(video_info, filename ='youtube_videos.csv')
-   
-            logging.info(f'Video data is saved in youtube_videos.csv')
-            print(f'Данные о видео сохранены в youtube_videos.csv')
-    except Exception as e:
-            logging.error(e)
-
-        
-
-def upload_to_drive(filename):
-    """Загружает файл в Google Drive."""
+    logging.info(f'upload_to_drive in Google Drive - {filename}')
     
     creds = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
@@ -123,10 +161,29 @@ def upload_to_drive(filename):
     file_metadata = {'name': filename}
     media = MediaFileUpload(filename, mimetype='text/csv')
     file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-    print(f'Файл загружен в Google Drive с ID: {file.get("id")}')
+    return f'Файл загружен в Google Drive с ID: {file.get("id")}'
 
+
+async def main():
+    """ Основная асинхронная функция которая получает информацию о данных для поиска видео,
+    после чего вызывает функцию search_youtube, осуществляет поиск, если все успешно вызывает функцию
+    gather_video_info для получения необходиых данных о видео.
+    После чего производиться запиьс данных в функции save_to_csv в файл формата csv
+    """
+    
+    query = input("Введите ключевое слово для поиска: ")
+    video_data = search_youtube(query)
+    try:
+        if video_data:
+            video_info = await gather_video_info(video_data)
+            print(save_to_csv(video_info, filename ='youtube_videos.csv'))
+            logging.info(f'Video data is saved in youtube_videos.csv')
+            print(upload_to_drive('youtube_videos.csv'))
+            logging.info(f'File is saved in Google Drive')
+    except Exception as e:
+            logging.error(f'error when searching and writing data: {e}')
+        
 
 if __name__ == "__main__":
     asyncio.run(main())
 
-    upload_to_drive('youtube_videos.csv')
